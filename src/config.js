d4dfbd869c0967aa2224979f1f4d1906849c36db
Virel Montes <jovanymontesv@gmail.com>
2020-07-04 09:32:20 -0700

    test db config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://virel@localhost/my_work_schedule',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://virel@localhost/my_work_schedule_test',
     JWT_SECRET: process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE1OTI1ODAzMDYsInN1YiI6InRlc3QtdXNlci0xIn0.yx0jkrCVTsi6p4y28-Eb0Swq-_85LVElECxwm11X4WE'
   }
